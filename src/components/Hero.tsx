1b01ade5789ffa93c7d8568d26cd54c0595cdee9
Inderjot Singh <inderjotsingh141@gmail.com>
2024-04-11 00:00:23 +0530

    working on hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Carousel } from './Carousel'
 import { Button } from './ui/button'
+import { HeroButton } from './HeroButton'
 
 
 export const Hero = () => {
@@ -14,17 +15,17 @@ export const Hero = () => {
                 <h3 className="text-xl" > Affordable way to build CustomGPT Bots, trained on your Data. <span className="text-orange-500 font-medium" > Using your own OpenAI API Key! </span>  </h3>
 
                 <div className="flex items-center flex-col " >
-                    <Button size={"lg"} >Get Started</Button>
+                    <HeroButton />
                     <span className="text-sm text-muted-foreground mt-3">Cancel Anytime</span>
                     <span className="text-sm text-muted-foreground"  >100+ Customers are saving on AI Bots</span>
                 </div>
 
-                <div className="flex flex-col items-center gap-4 text-sm mt-6" >
-                    <h2 className="p-3 w-1/2  bg-muted rounded-full" >✅  Unlimited Chats</h2>
+                <div className="flex flex-col md:flex-row items-center md:justify-center gap-4 text-sm mt-6" >
+                    <h2 className=" p-3 w-[300px] md:w-[250px] bg-muted rounded-full" >✅  Unlimited Chats</h2>
 
-                    <h2 className="p-3  w-1/2   bg-muted rounded-full" >🔐  Secure</h2>
+                    <h2 className="p-3 w-[300px]  md:w-[250px] bg-muted rounded-full" >🔐  Secure</h2>
 
-                    <h2 className="p-3  w-1/2  bg-muted rounded-full" >🧩 Biggest Library of Connectors</h2>
+                    <h2 className="p-3 w-[300px] md:w-[250px]  bg-muted rounded-full" >🧩 Biggest Library of Connectors</h2>
                 </div>
             </div>
         </div>
---
+++ src/components/HeroButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+
+export const HeroButton = () => {
+    return (
+        <Button className='bg-gradient-to-r from-purple-600 font-semibold w-[300px] text-white via-pink-400 to-orange-400  ' size={"lg"} >Get Started</Button>
+    )
+}
