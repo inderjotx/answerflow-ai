907f9d80fc79ab13f85b7bf85edbda663b77749e
Inderjot Singh <inderjotsingh141@gmail.com>
2024-04-12 00:01:09 +0530

    new features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <Hero />
 
       {/* features cards */}
-      <div className="w-full gap-20 flex flex-col items-center px-10 mt-8" >
+      <div className="w-full mb-28 gap-20 flex flex-col items-center px-10 mt-8" >
         <Card starLocation={StarLocation.End} />
         <Card starLocation={StarLocation.Middle} />
         <Card starLocation={StarLocation.Start} />
@@ -24,9 +24,9 @@ export default function Home() {
 
 
 
-      <div className="flex justify-center mt-40 px-20" >
-        <FeatureAnimation />
-      </div>
+      {/* <div className="flex justify-center mt-40 px-20" > */}
+      <FeatureAnimation />
+      {/* </div> */}
     </div>
   );
 }
--- src/components/FeatureAnimation.tsx
+++ src/components/FeatureAnimation.tsx
@@ -1,56 +1,98 @@
 'use client'
 
-import { motion, useMotionValueEvent, useScroll } from 'framer-motion'
+import { cn } from '@/lib/utils'
+import { Variants, motion, useMotionValueEvent, useScroll } from 'framer-motion'
 import React, { useEffect, useRef, useState } from 'react'
+import Image from 'next/image'
 
 type Props = {}
 
 const FEATURES = 4
 
+const data = {
+    "Data Analysis": "/analytics.jpg",
+    "Customer Support": "/support.jpg",
+    "Sales Analysis": "/sales.jpg",
+    "Product": "/mvp.jpg"
+}
+
+const variants = {
+    "enter": {
+        opacity: [0, 1],
+        y: [30, 0],
+        z: [2, 0],
+        transition: { duration: 0.5 }
+    },
+    "exit": {
+        opacity: [1, 0],
+        y: [0, 30],
+        z: [0, 2],
+        transition: { duration: 0.5 }
+    }
+
+} satisfies Variants
+
 export const FeatureAnimation = (props: Props) => {
 
-    const height = `h-[${FEATURES * 100}vh]`
-    const [curFeatNum, setCurFeatNum] = useState<number>(0)
-
-    const parentRef = useRef<HTMLDivElement | null>(null)
-
-    const { scrollYProgress, scrollY } = useScroll({
-        container: parentRef,
-        offset: ["end end", "start start"]
-    })        // latestVal % size  
+    const height = `h-[${100 * FEATURES}vh]`
+    const ref = useRef<HTMLDivElement | null>(null)
 
 
-    useEffect(() => {
 
-        console.log("changed vale ")
+    const [activeNum, setActiveNum] = useState<number>(0)
+    const [activeVariant, setActiveVariant] = useState<"enter" | "exit">("enter")
 
-    }, [scrollYProgress])
-
-
-    useMotionValueEvent(scrollYProgress, "change", (latestVal) => {
-
-        const windowSize = 1 / FEATURES
-        const curActiveFeature = latestVal % windowSize
-        console.log(latestVal)
-        console.log(curActiveFeature)
-        // this event is not being triggered
+    const keys = Object.keys(data)
+    process
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start start", "end end"]
 
     })
 
+
+
+    useMotionValueEvent(scrollYProgress, "change", (val) => {
+
+        const eachSlide = 1 / FEATURES
+        const activeIndex = Math.ceil(val / eachSlide) - 1
+        console.log(activeIndex)
+
+        if (activeIndex != activeNum && activeIndex != -1) {
+            setActiveNum(activeIndex)
+        }
+    })
+
     return (
-        <div ref={parentRef} className='w-full' >
-            <motion.div className={`${height} w-full`} >
+        <motion.div ref={ref} className={`h-[400vh] border  `} >
 
-
-
-                <div className='border flex justify-around sticky top-[25%] w-full ' >
-
-
-                    <div>features</div>
-                    <div> cards </div>
+            <div className='sticky border flex top-[130px] w-4/5 mx-auto h-[450px]  '>
+                <div className='flex flex-col h-full'>
+                    <motion.div className='relative w-1 h-full bg-yellow-400 rounded-b-full' style={{ scaleY: scrollYProgress, transformOrigin: "top" }} >
+                    </motion.div>
                 </div>
-            </motion.div>
 
-        </div>
+                <div className='flex flex-col my-10 mx-8 text-muted-foreground justify-between'>
+                    {
+                        keys.map((key) => (
+                            <div className={cn('text-3xl', key === keys[activeNum] && "text-white")} key={key}>{key}</div>
+                        ))
+                    }
+                </div>
+
+                <div className='flex items-center ml-auto pr-28' >
+                    <motion.div
+                        variants={variants}
+                        animate={activeVariant}
+                        className='relative w-[450px] h-[300px] overflow-hidden rounded-md '>
+                        <Image
+                            className='absolute inset-0 object-cover'
+                            src={data[keys[activeNum] as keyof typeof data]} fill sizes='100' alt='image-showing-information' />
+                    </motion.div>
+
+                </div>
+            </div>
+
+        </motion.div>
     )
 }
